7b19f8133c61977f66b1de742815af258e5b4e3a
Matthew Salomon <matthew.h.salomon@gmail.com>
2020-02-02 02:40:55 -0500

    folders and shit

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer background;
+    public Camera cam;
+    void Start()
+    {
+
+        float orthoSize = background.bounds.size.x * Screen.height / Screen.width * 0.5f;
+        cam.main.orthographicSize = orthoSize;
+    }
+
+
+}
