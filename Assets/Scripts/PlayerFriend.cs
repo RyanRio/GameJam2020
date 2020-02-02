faa25896ea798cc6a644c75b3cbd0167f66ef2ef
kriti-g <kriti7000@gmail.com>
2020-02-02 03:31:36 -0500

    PlayerFriend :)

---
+++ Assets/Scripts/PlayerFriend.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFriend : MonoBehaviour
+{
+    public float speed;
+    public RigidBody2D rb;
+    private Vector2 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // the good
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.DeltaTime);
+    }
+}
