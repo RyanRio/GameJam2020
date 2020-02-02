42081a58bd5c702660d7437b0683b77c7689d09a
Maggie Van Nortwick <maggievannortwick@yahoo.com>
2020-02-02 16:16:56 -0500

    text box

---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextBox : Interactable
+{
+    public string content;
+    public Text text;
+    private TextBox next;
+
+    public override void interact()
+    {
+        text.text = content;
+        Vector3 t = transform.position;
+        t.x += 5;
+        text.transform.position = t;
+    }
+}
+ 
