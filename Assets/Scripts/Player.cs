307fc230877c7f8c7b8caf58f6e11a8e306f9309
RyanRio <thermitchellman@gmail.com>
2020-02-02 13:48:18 -0500

    intro scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,32 +7,56 @@ public class Player : MonoBehaviour
 
     public Rigidbody2D rb;
     public float speed;
-    private Vector2 direction;
+    private Vector2 prevDirection;
+    public int interactDistance;
 
     public LayerMask interactable;
+    private bool e = false;
+
     // Start is called before the first frame update
     void Start()
     {
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
-        rb.MovePosition(rb.position + direction.normalized * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.E))
+        {
+            e = true;
+        }
+        else if (e)
+        {
+            e = false;
+        }
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(x) > Mathf.Epsilon || Mathf.Abs(y) > Mathf.Epsilon)
+        {
+            print("moving");
+            prevDirection.x = x;
+            prevDirection.y = y;
+        }
+
+        rb.MovePosition(rb.position + new Vector2(x, y).normalized * Time.deltaTime * speed);
         BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
 
-        if(Input.GetKeyUp(KeyCode.E))
+        if(e)
         {
+            Vector2 end = transform.position + new Vector3(interactDistance * prevDirection.x,interactDistance * prevDirection.y);
+            print("start" + transform.position);
+            print("end: " + end);
+            Debug.DrawLine(transform.position, end, Color.red, 100f);
+
             print("hit e");
             boxCollider.enabled = false;
             RaycastHit2D hit;
-            HitLayerObject.Hit(interactable, transform.position, new Vector2(transform.position.x, transform.position.y) + new Vector2(5, 0), out hit);
+            HitLayerObject.Hit(interactable, transform.position, end, out hit);
 
             if (hit.transform != null)
             {
-                Debug.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y) + new Vector2(5, 0), Color.red, 100f);
                 Interactable comp = hit.transform.GetComponent<Interactable>();
                 comp.interact();
             }
